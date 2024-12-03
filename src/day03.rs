5c3d620ca701ac3332e7cee1a84d42485c0ddd2a
gustavo.nagel <gust.nagel@gmail.com>
2024-12-03 19:00:39 -0300

    Response for day 3

--- src/day03.rs
+++ src/day03.rs
@@ -15,37 +15,32 @@ pub fn run(contents: String, part: &i8) {
 
     } else if part == &2 {
         let re = Regex::new(r"(?<instruction>mul|don\'t|do)\((?<params>\d{0,3},?\d{0,3})\)").unwrap();
-        let final_results = contents.lines().map(|line| {
-            let mut enabled = true;
-            let mut line_results_sum = 0;
-            for caps in re.captures_iter(line) {
-                let instruction = caps.name("instruction").unwrap().as_str();
-                let params = caps.name("params").unwrap().as_str();
-                println!("{} {}", instruction, params);
-                match instruction {
-                    "mul" => {
-                        if enabled {
-                          let splitted: Vec<&str> = params.split(",").collect();
-                          if splitted.len() == 2 {
-                              let a: i32 = splitted[0].parse().unwrap();
-                              let b: i32 = splitted[1].parse().unwrap();
-                              line_results_sum += a * b;
-                            //   println!("{} x {} = {}", a, b, a * b);
-                          }
-                        //   println!("{:?}", splitted);
+        let mut enabled = true;
+        let mut line_results_sum = 0;
+        for caps in re.captures_iter(&contents) {
+            let instruction = caps.name("instruction").unwrap().as_str();
+            let params = caps.name("params").unwrap().as_str();
+            println!("{} {}", instruction, params);
+            match instruction {
+                "mul" => {
+                    if enabled {
+                        let splitted: Vec<&str> = params.split(",").collect();
+                        if splitted.len() == 2 {
+                            let a: i32 = splitted[0].parse().unwrap();
+                            let b: i32 = splitted[1].parse().unwrap();
+                            line_results_sum += a * b;
                         }
                     }
-                    "don't" => {
-                        if params.is_empty() {enabled = false;}
-                    }
-                    "do" => {
-                        if params.is_empty() {enabled = true;}
-                    }
-                    _ => {}
                 }
+                "don't" => {
+                    if params.is_empty() {enabled = false;}
+                }
+                "do" => {
+                    if params.is_empty() {enabled = true;}
+                }
+                _ => {}
             }
-            line_results_sum
-        });
-        println!("Response: {}", final_results.sum::<i32>());
+        }
+        println!("Response: {}", line_results_sum);
     }
 }
