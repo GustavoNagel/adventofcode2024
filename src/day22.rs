61427a9184b0c6fccc108dbd69d9b216163de61c
gustavo.nagel <gust.nagel@gmail.com>
2025-01-22 18:21:10 -0300

    Solution day 22 part 2

--- src/day22.rs
+++ src/day22.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use counter::Counter;
+
 fn mix(secret_number: i64, value: i64) -> i64 {
     secret_number ^ value
 }
@@ -19,15 +23,38 @@ fn calculate_secret_key(old_secret_key: i64) -> i64 {
 }
 
 pub fn run(contents: String, part: &i8) {
-    let total_sum: i64 = contents.lines().map(|line| {
-        let mut secret_key: i64 = line.parse().unwrap();
-        for _ in 0..2000 {
-            secret_key = calculate_secret_key(secret_key);
-        }
-        println!("Line {} Secret key: {}", line, secret_key);
-        secret_key
-    }).sum();
-    println!("Response Part 1: {}", total_sum);
+    if part == &1 {
+        let total_sum: i64 = contents.lines().map(|line| {
+            let mut secret_key: i64 = line.parse().unwrap();
+            for _ in 0..2000 {
+                secret_key = calculate_secret_key(secret_key);
+            }
+            // println!("Line {} Secret key: {}", line, secret_key);
+            secret_key
+        }).sum();
+        println!("Response Part 1: {}", total_sum);
+    } else if part == &2 {
+        let mut global_counter: Counter<Vec<i8>> = Counter::new();
+        contents.lines().for_each(|line| {
+            let mut monkey_prices = HashMap::new();
+            let mut secret_key: i64 = line.parse().unwrap();
+            let mut unit_values = vec![(secret_key % 10) as u8];
+            for i in 0..2000 {
+                secret_key = calculate_secret_key(secret_key);
+                unit_values.push((secret_key % 10) as u8);
+                if i >= 4 {
+                    let last_values = &unit_values[i-4..i+1];
+                    let last_diffs: Vec<i8> = last_values.windows(2).map(|w| w[1] as i8 - w[0] as i8).collect();
+                    if !monkey_prices.contains_key(&last_diffs) {
+                        monkey_prices.insert(last_diffs.clone(), last_values[4]);
+                        global_counter[&last_diffs] += last_values[4] as usize;
+                    }
+                    // println!("{:?}: {:?}", last_diffs, last_values[4]);
+                }
+            }
+        });
+        println!("Response Part 2: {:?}", global_counter.k_most_common_ordered(1)[0]);
+    }
 }
 
 #[cfg(test)]
